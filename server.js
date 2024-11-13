e268e4796fb6ff6cd2b8d5828dd49bad20a8cb9c
MaximilianSmith95 <39773744+MaximilianSmith95@users.noreply.github.com>
2024-11-13 19:46:35 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,7 +15,7 @@ app.use((req, res, next) => {
     }
     next();
 });
-app.set('trust proxy', true);
+
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
