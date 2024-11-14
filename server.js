bb885bfa163469f11660a9692395adf5f45896a5
MaximilianSmith95 <39773744+MaximilianSmith95@users.noreply.github.com>
2024-11-14 11:03:47 +0000

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,18 @@ app.use((req, res, next) => {
     }
     next();
 });
+const mysql = require('mysql2/promise');
 
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
